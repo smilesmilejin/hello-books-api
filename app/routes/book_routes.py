f3c9522359d37257da991094ddfc91afa5152cbf
Xinshuang Jin <jinxinshuang@gmail.com>
2025-04-15 00:29:05 -0400

    add helper function

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response
 from app.models.book import books
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
@@ -20,18 +20,27 @@ def get_all_books():
 
 @books_bp.get("/<book_id>")
 def get_one_book(book_id):
+    book = validate_book(book_id)
+
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description,
+    }
+
+
+def validate_book(book_id):
     try:
         book_id = int(book_id)
-    except ValueError:
-        return {"message": f"book {book_id} invalid"}, 400
-    
+    except:
+        # When book_id is not a number
+        response = {"message": f"book {book_id} invalid"}
+        abort(make_response(response, 400))
+
     for book in books:
         if book.id == book_id:
-            return {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description,
-            }
-        
-    return {"message": f"book {book_id} not found"}, 404
+            return book
 
+    # When book_id does not exist in books
+    response = {"message": f"book {book_id} not found"}
+    abort(make_response(response, 404))

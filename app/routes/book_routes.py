558903b2da02d25c52691b33ae3e90a6078c32b0
Xinshuang Jin <jinxinshuang@gmail.com>
2025-04-15 00:20:22 -0400

    changes based on Error Handling

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -17,3 +17,21 @@ def get_all_books():
             }
         )
     return books_response
+
+@books_bp.get("/<book_id>")
+def get_one_book(book_id):
+    try:
+        book_id = int(book_id)
+    except ValueError:
+        return {"message": f"book {book_id} invalid"}, 400
+    
+    for book in books:
+        if book.id == book_id:
+            return {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description,
+            }
+        
+    return {"message": f"book {book_id} not found"}, 404
+

ae41323b5f6034f8997fd09b46a74723ec51e1c9
Xinshuang Jin <jinxinshuang@gmail.com>
2025-04-22 17:29:34 -0400

    03b-create

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, abort, make_response
 
 ################## 03 Building an API: comment the following
+# from flask import Blueprint, abort, make_response
 # from app.models.book import books
 
-books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
+# books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
 
 
@@ -51,3 +51,29 @@ books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 #     abort(make_response(response, 404))
 
 ################## END 03 Building an API: comment the following
+
+
+################## 03 Building an API:Create
+
+from flask import Blueprint, abort, make_response, request
+from app.models.book import Book
+from ..db import db
+
+books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
+
+@books_bp.post("")
+def create_book():
+    request_body = request.get_json() # use request to accesss to incoming request data
+    title = request_body["title"]
+    description = request_body["description"]
+
+    new_book = Book(title=title, description=description)
+    db.session.add(new_book) # Add new_book to the database
+    db.session.commit() # database commit the changes
+
+    response = {
+        "id": new_book.id,
+        "title": new_book.title,
+        "description": new_book.description,
+    }
+    return response, 201

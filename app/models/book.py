79ac87ce63bef7baa4985ff03943a023ef86316c
Xinshuang Jin <jinxinshuang@gmail.com>
2025-05-02 17:04:06 -0400

    08b-nested-routes

--- app/models/book.py
+++ app/models/book.py
@@ -1,71 +1,112 @@
-################## 03 Building an API: Comment the hardcoded books data 
-# class Book:
-#     def __init__(self, id, title, description):
-#         self.id = id
-#         self.title = title
-#         self.description = description
+# ################## 03 Building an API: Comment the hardcoded books data 
+# # class Book:
+# #     def __init__(self, id, title, description):
+# #         self.id = id
+# #         self.title = title
+# #         self.description = description
 
-# books = [
-#     Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
-#     Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
-#     Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
-# ]
+# # books = [
+# #     Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
+# #     Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
+# #     Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
+# # ]
 
- ############## End 03 Building an API: Comment the hardcoded books data 
+#  ############## End 03 Building an API: Comment the hardcoded books data 
 
-################## Added from 03 Building an API
-# from sqlalchemy.orm import Mapped, mapped_column # This file needs access to SQLAlchemy's tools for defining table columns in a model
+# ################## Added from 03 Building an API
+# # from sqlalchemy.orm import Mapped, mapped_column # This file needs access to SQLAlchemy's tools for defining table columns in a model
+# # from ..db import db
+# ##################### Added from 08 Building an API one-to-many Part 1
+# from sqlalchemy.orm import Mapped, mapped_column, relationship
+# from sqlalchemy import ForeignKey
+# from typing import Optional
 # from ..db import db
-##################### Added from 08 Building an API one-to-many Part 1
+# ##################### End from 08 Building an API one-to-many Part 1
+
+# # By default, SQLAlchemy will use the lowercase version of this class name as the name of the table it will create.
+# class Book(db.Model):
+
+#     # Added from 07 Building an API - Refactoring
+#     # Complete from_dict function example
+#     # indented under the Book class definition
+#     @classmethod
+#     def from_dict(cls, book_data):
+#         new_book = Book(title=book_data["title"],
+#                         description=book_data["description"])
+#         return new_book
+#     # End from 07 Building an API - Refactoring
+
+#     # Instances of Book will have an attribute id, which maps to a database column of type int. 
+#     # Notice that this attribute goes outside of any instance method and doesn't reference self.
+#     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    
+#     # Creates a required title attribute, which maps to a string column, title
+#     title: Mapped[str]
+    
+#     # Creates a required description attribute, which maps to a string column, description
+#     # To mark a text column as nullable, we would need to add Optional to the type hint, written as Mapped[Optional[str]].
+#     description: Mapped[str]
+
+#     ##################### Added from 08 Building an API one-to-many Part 1
+#     # For example, while Mapped[Optional[int]] declares a nullable integer column, 
+#     # Mapped[int] declares an integer column that disallows NULL values, just as Mapped[str] declares a required string column.
+#     # Defining Relationships as Optional or Required
+#     author_id: Mapped[Optional[int]] = mapped_column(ForeignKey("author.id"))
+#     author: Mapped[Optional["Author"]] = relationship(back_populates="books")
+
+#     ##################### End from 08 Building an API one-to-many Part 1
+
+# # If deleting rows with id 1, the next insert will be 2, run the folloiwng query to reset autoincrement in 1 
+#     # SELECT pg_get_serial_sequence('book', 'id');
+#     # ALTER SEQUENCE public.book_id_seq RESTART WITH 1;
+
+#     # Added from 07 Building an API - Refactoring Part 2
+#     def to_dict(self):
+#         book_as_dict = {}
+#         book_as_dict["id"] = self.id
+#         book_as_dict["title"] = self.title
+#         book_as_dict["description"] = self.description
+
+#         return book_as_dict
+#     # End from 07 Building an API - Refactoring Part 2
+
+
+##################### Added from 08 Building an API one-to-many Part 2
+# app/models/book.py
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey
 from typing import Optional
 from ..db import db
-##################### End from 08 Building an API one-to-many Part 1
 
-# By default, SQLAlchemy will use the lowercase version of this class name as the name of the table it will create.
 class Book(db.Model):
-
-    # Added from 07 Building an API - Refactoring
-    # Complete from_dict function example
-    # indented under the Book class definition
-    @classmethod
-    def from_dict(cls, book_data):
-        new_book = Book(title=book_data["title"],
-                        description=book_data["description"])
-        return new_book
-    # End from 07 Building an API - Refactoring
-
-    # Instances of Book will have an attribute id, which maps to a database column of type int. 
-    # Notice that this attribute goes outside of any instance method and doesn't reference self.
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    
-    # Creates a required title attribute, which maps to a string column, title
     title: Mapped[str]
-    
-    # Creates a required description attribute, which maps to a string column, description
-    # To mark a text column as nullable, we would need to add Optional to the type hint, written as Mapped[Optional[str]].
     description: Mapped[str]
-
-    ##################### Added from 08 Building an API one-to-many Part 1
-    # For example, while Mapped[Optional[int]] declares a nullable integer column, 
-    # Mapped[int] declares an integer column that disallows NULL values, just as Mapped[str] declares a required string column.
-    # Defining Relationships as Optional or Required
     author_id: Mapped[Optional[int]] = mapped_column(ForeignKey("author.id"))
     author: Mapped[Optional["Author"]] = relationship(back_populates="books")
 
-    ##################### End from 08 Building an API one-to-many Part 1
-
-# If deleting rows with id 1, the next insert will be 2, run the folloiwng query to reset autoincrement in 1 
-    # SELECT pg_get_serial_sequence('book', 'id');
-    # ALTER SEQUENCE public.book_id_seq RESTART WITH 1;
-
-    # Added from 07 Building an API - Refactoring Part 2
     def to_dict(self):
         book_as_dict = {}
         book_as_dict["id"] = self.id
         book_as_dict["title"] = self.title
         book_as_dict["description"] = self.description
 
+        if self.author:
+            book_as_dict["author"] = self.author.name
+
         return book_as_dict
-    # End from 07 Building an API - Refactoring Part 2
+    
+    @classmethod
+    def from_dict(cls, book_data):
+        # Use get() to fetch values that could be undefined to avoid raising an error
+        author_id = book_data.get("author_id")
+
+        new_book = cls(
+            title=book_data["title"],
+            description=book_data["description"],
+            author_id=author_id
+        )
+
+        return new_book
+    
+##################### End from 08 Building an API one-to-many Part 2
--- app/routes/author_routes.py
+++ app/routes/author_routes.py
@@ -1,7 +1,10 @@
+##################### Added 08 Building an API one-to-many Part 1
 # app/routes/author_routes.py
 from flask import Blueprint, request, make_response, abort
 from app.models.author import Author
 from ..db import db
+from app.routes.route_utilities import validate_model
+from app.models.book import Book
 
 bp = Blueprint("authors_bp", __name__, url_prefix="/authors")
 
@@ -34,3 +37,37 @@ def get_all_authors():
     authors_response = [author.to_dict() for author in authors]
 
     return authors_response
+
+##################### Added 08 Building an API one-to-many Part 2
+# Complete POST /authors/<author_id>/books endpoint example and updated book.py
+# POSTing a Book to an Author
+@bp.post("/<author_id>/books")
+def create_book_with_author(author_id):
+    author = validate_model(Author, author_id)
+    
+    request_body = request.get_json()
+    request_body["author_id"] = author.id
+
+    try:
+        new_book = Book.from_dict(request_body)
+
+    except KeyError as error:
+        response = {"message": f"Invalid request: missing {error.args[0]}"}
+        abort(make_response(response, 400))
+        
+    db.session.add(new_book)
+    db.session.commit()
+
+    return make_response(new_book.to_dict(), 201)
+
+# GETting All Books from an Author
+# As a client, I want to send a request to read all books by a particular author in the database.
+# Complete GET /authors/<author_id>/books endpoint example
+# app/routes/author_routes.py
+@bp.get("/<author_id>/books")
+def get_books_by_author(author_id):
+    author = validate_model(Author, author_id)
+    response = [book.to_dict() for book in author.books]
+    return response
+
+##################### End  08 Building an API one-to-many Part 2
